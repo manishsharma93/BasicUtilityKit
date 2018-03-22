acd8f1385e66f900daa8b8ca87522dd84e4721d4
Manish Sharma <manishsharma93@hotmail.com>
2018-03-22 17:20:41 +0530

    Add files via upload

--- BasicUtilityKit.framework/Headers/BasicUtility.h
+++ BasicUtilityKit.framework/Headers/BasicUtility.h
@@ -12,12 +12,35 @@
 @interface BasicUtility : NSObject
 
 + (instancetype)sharedInstance;
-
+/*
+ @Method registerForPushNotifications: this method is used to register for Push Notifications
+ @return - void
+ */
 +(void)registerForPushNotifications;
+/*
+ @Method getDeviceTokenString: this method is used to get device token string value
+ @return - String
+ */
 +(NSString *)getDeviceTokenString:(NSData *)deviceToken;
+/*
+ @Method removeBlankSpaceValue: this method is used to remove NULL or Nil values from Stings
+ @return - String
+ */
 +(NSString *)removeBlankSpaceValue:(id)val;
 
+/**********************************
+*****RICH PUSH NOTIFICATION METHODS*****
+
+***********************************/
+/*
+ @Method didReceiveNotificationRequest: this method is used to get the Rich Push Request
+ @return - void
+ */
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withAttachmentURL:(NSString *)mediaURL withContentHandler:(void (^)(UNNotificationContent *))contentHandler ;
+/*
+ @Method serviceExtensionTimeWillExpire: this method will be called if the service Extention Time will Expire
+ @return - void
+ */
 - (void)serviceExtensionTimeWillExpire;
 
 @end
