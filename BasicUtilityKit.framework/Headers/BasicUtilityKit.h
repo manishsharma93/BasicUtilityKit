5fad5a3b9c5265841548846feead585273ed838e
Manish Sharma <manishsharma93@hotmail.com>
2018-03-22 17:09:50 +0530

    Add files via upload

---
+++ BasicUtilityKit.framework/Headers/BasicUtilityKit.h
@@ -0,0 +1,18 @@
+//
+//  BasicUtilityKit.h
+//  BasicUtilityKit
+//
+//  Created by Manish Kumar on 22/03/18.
+//  Copyright © 2018 Manish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BasicUtility.h"
+
+//! Project version number for BasicUtilityKit.
+FOUNDATION_EXPORT double BasicUtilityKitVersionNumber;
+
+//! Project version string for BasicUtilityKit.
+FOUNDATION_EXPORT const unsigned char BasicUtilityKitVersionString[];
+
+
